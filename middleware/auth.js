78073cb66e352bd0e412d8f411471effd5cc9c52
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-10 15:51:35 -0300

    Auth Middleware & Protecting Routes

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// funcção middleware = vai proteger as rotas privadas
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // check if not token
+  if (!token) {
+    //401 non authorized
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
