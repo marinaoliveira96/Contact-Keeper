832179dc8c23b27cdfb9c657f8724d13eef85ad8
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-10 16:04:23 -0300

    Contact Model & Get Contacts Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @routes  GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contatcts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+
+    res.json(contatcts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @routes  POST api/auth
 // @desc    Add new contact
 // @access  Purivate
 router.post('/', (req, res) => {
-  res.send('Add contacts');
+  res.send('add contact');
 });
 
 // @routes  PUT api/contacts/:id
