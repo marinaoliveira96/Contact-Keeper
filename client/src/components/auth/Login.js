fd3b619f1275dda8e30c29e591c15bc576b5cd4c
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-23 15:00:01 -0300

    User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () => {
+const Login = (props) => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
+    if (error === 'Invalid Credentials') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: '',
     password: '',
   });
 
-  const onChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
+  const { email, password } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log('Login submit');
+    if (email === '' || password === '') {
+      setAlert('Please fill in all fields', 'danger');
+    } else {
+      login({
+        email,
+        password,
+      });
+    }
   };
 
-  const { email, password } = user;
   return (
     <div className="form-container">
       <h1>
@@ -24,7 +50,13 @@ const Login = () => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
@@ -33,13 +65,13 @@ const Login = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
           />
         </div>
-
         <input
           type="submit"
           value="Login"
-          className="btn- btn-primary btn-block"
+          className="btn btn-primary btn-block"
         />
       </form>
     </div>
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -14,7 +14,7 @@ import {
   CLEAR_ERRORS,
 } from '../types';
 
-const AuthtState = (props) => {
+const AuthState = (props) => {
   const initialState = {
     token: localStorage.getItem('token'),
     isAuthenticated: null,
@@ -25,19 +25,21 @@ const AuthtState = (props) => {
 
   const [state, dispatch] = useReducer(authReducer, initialState);
 
-  // Load user
+  // Load User
   const loadUser = async () => {
     if (localStorage.token) {
       setAuthToken(localStorage.token);
     }
+
     try {
       const res = await axios.get('/api/auth');
 
-      dispatch({ type: USER_LOADED, payload: res.data });
-    } catch (err) {
       dispatch({
-        type: AUTH_ERROR,
+        type: USER_LOADED,
+        payload: res.data,
       });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
     }
   };
 
@@ -65,12 +67,36 @@ const AuthtState = (props) => {
       });
     }
   };
+
   // Login User
-  const login = () => console.log('login');
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
+
   // Logout
   const logout = () => console.log('logout');
 
-  // clear erros
+  // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
@@ -93,4 +119,4 @@ const AuthtState = (props) => {
   );
 };
 
-export default AuthtState;
+export default AuthState;
