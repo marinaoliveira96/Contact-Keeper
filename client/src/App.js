c5e0fab03f2636db221782fc049bd7e6d7699a6e
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-24 09:38:13 -0300

    PrivateRoute Component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -12,6 +13,10 @@ import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -28,10 +33,10 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/about" component={About} />
-                  <Route path="/register" component={Register} />
-                  <Route path="/login" component={Login} />
+                  <PrivateRoute exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
                 </Switch>
               </div>
             </>
